b8c233a9d6906cc1a6f2556b6bacf9bee10ad762
akhramova <xaa2128@mail.ru>
2024-01-18 22:31:02 +0300

    Задание 2

---
+++ src/Filter.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем фильтрацию");
+        List<Integer> result = new ArrayList<>();
+        for (Integer number : source) {
+            if (number >= treshold) {
+                result.add(number);
+                logger.log("Элемент \"" + number + "\" проходит");
+            } else {
+                logger.log("Элемент \"" + number + "\" не проходит");
+            }
+        }
+        logger.log("Прошло фильтр " + result.size() + " элемента из " + source.size());
+        return result;
+    }
+}
